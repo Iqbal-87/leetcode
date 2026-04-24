18fc810cb4315a45b7defaee91e8ded655bc56cc
Tabish Iqbal <93008376+Iqbal-87@users.noreply.github.com>
2026-04-24 13:50:51 +0530

    Time: 1 ms (100%), Space: 43.4 MB (62.87%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.java
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -2,8 +2,8 @@ class Solution {
     public int maxPower(String s) {
         int max=1;
         int count=1;
-        for(int i=1;i<s.length();i++){
-            if(s.charAt(i)==s.charAt(i-1)){
+        for(int i=0;i<s.length()-1;i++){
+            if(s.charAt(i)==s.charAt(i+1)){
                 count++;
                 max=Math.max(count,max);
             }
