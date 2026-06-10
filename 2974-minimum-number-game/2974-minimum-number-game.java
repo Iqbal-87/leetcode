58efcac2818a24c4962a9fe2e2e4717a57bcfd68
Tabish Iqbal <93008376+Iqbal-87@users.noreply.github.com>
2026-06-10 17:44:42 +0530

    Time: 6 ms (30.58%), Space: 47.1 MB (57.39%) - LeetHub

---
+++ 2974-minimum-number-game/2974-minimum-number-game.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] numberGame(int[] nums) {
+        int n = nums.length;
+        int[] arr = new int[n];
+        Arrays.sort(nums);
+        for(int i=0;i<n;i=i+2){
+            arr[i+1]= nums[i];
+        }
+        for(int i=1;i<n;i=i+2){
+            arr[i-1] =nums[i];
+        }
+        return arr;
+        
+    }
+}
